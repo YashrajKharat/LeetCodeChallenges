ef3b311f8c6de3052199bc32201316f3f746ecfe
YashrajKharat <Kharatyashraj2007@gmail.com>
2026-07-03 19:40:31 +0530

    Time: 0 ms (100%), Space: 28.1 MB (88.91%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -13,14 +13,40 @@ public:
         //     }
         // }
 
-        map<int,int> mpp;
-        for(int i=0;i<nums.size();i++){
-            mpp[nums[i]]++;
-        }
-        for(auto it:mpp){
-            if(it.second>nums.size()/2){
-                return it.first;
+        // map<int,int> mpp;
+        // for(int i=0;i<nums.size();i++){
+        //     mpp[nums[i]]++;
+        // }
+        // for(auto it:mpp){
+        //     if(it.second>nums.size()/2){
+        //         return it.first;
+        //     }
+        // }
+
+
+        int cnt=0;
+        int cnt1=0;
+        int el;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(cnt==0){
+                cnt=1;
+                el=nums[i];
             }
+            else if(nums[i]==el){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]==el){
+                cnt1++;
+            }
+        }
+        if(cnt1>n/2){
+            return el;
         }
         return{};
     }
